eeb7860978b18f06b96c1f76486b7bcd790dc31f
Test_name <test_email@example.com>
2024-11-10 22:00:54 +0700

    Были доработаны формы(но не до конца)

--- frontend/app/src/main/java/com/example/gith_the_architect/MainActivity.kt
+++ frontend/app/src/main/java/com/example/gith_the_architect/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.gith_the_architect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import android.content.Intent
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +20,63 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login);
         val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+
+        var backgroundThread = HandlerThread("NetworkThread")
+        backgroundThread.start()
+        var backgroundHandler = Handler(backgroundThread.looper)
+
+        button.setOnClickListener() {
+            Log.d("MainActivity", "Кнопка нажата")
+            val editText: EditText = findViewById(R.id.editText)
+            val telephone: String = editText.text.toString()
+
+            if (!telephone.isEmpty()) {
+
+                val client = OkHttpClient()
+
+                val request = Request.Builder()
+                    .url("http://192.168.181.71:8080/api/user/authorization/" + telephone)
+                    .build()
+
+                backgroundHandler.post {
+                    try {
+                        client.newCall(request).execute().use { response ->
+                            if (!response.isSuccessful) {
+                                runOnUiThread {
+                                    Toast.makeText(
+                                        this@MainActivity,
+                                        "Такого пользователя не существует",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                                throw IOException(
+                                    "Запрос к серверу не был успешен:" +
+                                            " ${response.code} ${response.message}"
+                                )
+                            } else {
+                                Log.d("MainActivity", "Server: ${response.header("Server")}")
+                                Log.d("MainActivity", response.body!!.string())
+
+                                runOnUiThread {
+                                    val intent = Intent(this@MainActivity, MenuActivity::class.java)
+                                    startActivity(intent)
+                                }
+                            }
+                        }
+                    } catch (e: IOException) {
+                        Log.e("MainActivity", "Ошибка подключения: $e")
+                    }
+                }
+            }
+            else {
+                runOnUiThread {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Введите номер телефон",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
         }
     }
 }
--- frontend/app/src/main/java/com/example/gith_the_architect/SecondActivity.kt
+++ frontend/app/src/main/java/com/example/gith_the_architect/SecondActivity.kt
@@ -3,9 +3,11 @@ package com.example.gith_the_architect
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-
+import java.net.URL
+import java.net.HttpURLConnection
 
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,24 @@ class SecondActivity : ComponentActivity() {
         setContentView(R.layout.activity_registration);
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
+            val editText: EditText = findViewById(R.id.editText)
+            val firstname: String = editText.text.toString()
+            val editText2: EditText = findViewById(R.id.editText)
+            val secondname: String = editText2.text.toString()
+            val editText3: EditText = findViewById(R.id.editText)
+            val telephone: String = editText3.text.toString()
+            val url = URL("http://100.104.66.125:8080/api/user/")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+
+            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                val responseBody = connection.inputStream.bufferedReader().use { it.readText() }
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+                println("Ответ: $responseBody")
+            } else {
+                println("Ошибка: ${connection.responseCode}")
+            }
             val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
